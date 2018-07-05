a9ed97ba2d97371f5831c194e5a25641e4589419
Damien Renaud <damien.renaud@soprasteria.com>
2018-07-05 10:50:38 +0200

    Structure for test

---
+++ ws-b/src/main/java/hello/WsBApplication.java
@@ -0,0 +1,12 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsBApplication.class, args);
+	}
+}
