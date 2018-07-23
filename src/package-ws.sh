93b9d196beee81f834cf2240cc924f50548f543e
Damien Renaud <damien.renaud@soprasteria.com>
2018-07-23 15:14:24 +0200

    Modification pour poc

--- src/package-ws.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-docker run --rm -it -v $(pwd)/ws-a:/project -w /project maven mvn package
-docker run --rm -it -v $(pwd)/ws-b:/project -w /project maven mvn package
-docker run --rm -it -v $(pwd)/ws-c:/project -w /project maven mvn package
-docker run --rm -it -v $(pwd)/ws-d:/project -w /project maven mvn package
